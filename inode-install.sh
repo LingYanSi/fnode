55a2bc038df822607744af7a914f7df2e072f665
wangyong <1029891106@qq.com>
2017-06-22 23:11:23 +0800

    fix install bug

--- inode-install.sh
+++ inode-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash/
 
 p=/usr/local/bin/inode
-wget -O $p https://lingyansi.github.io/inode/inode.sh
+rm /usr/local/bin/inode
+wget -o $p https://lingyansi.github.io/inode/inode.sh
 chmod 777 $p
